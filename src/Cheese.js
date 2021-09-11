26fbc352272288a11bb1b7576cf0b7ba4476e82e
Yongyeon Kim <yeonlotus@ewhain.net>
2021-09-11 11:57:26 +0900

    Create first component

---
+++ src/Cheese.js
@@ -0,0 +1,5 @@
+function Cheese() {
+  return <h3>I love Cheese</h3>;
+}
+
+export default Cheese;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Cheese from "./Cheese";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Cheese />
   </React.StrictMode>,
   document.getElementById("root")
 );
