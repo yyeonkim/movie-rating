c9eb279f377c9175b72e810bcd8ef94a9c9f57e7
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-12 16:21:18 +0900

    setState() and current

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends React.Component {
   };
 
   add = () => {
-    console.log("Add");
+    this.setState((current) => ({ count: current.count + 1 }));
   };
   minus = () => {
-    console.log("Minus");
+    this.setState({ count: this.state.count - 1 });
   };
 
   render() {
