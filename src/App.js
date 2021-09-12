0f90bae29d7668630cfb49c5e492809e1a23d539
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-12 16:58:07 +0900

    Component life cycle
    component mouting, updating, unmouting 될 때 실행되는 함수:
    constructor, componentDidMount, componentDidUpdate, componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import PropTypes from "prop-types";
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
   state = {
     count: 0,
   };
@@ -12,6 +16,14 @@ class App extends React.Component {
   minus = () => {
     this.setState({ count: this.state.count - 1 });
   };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
 
   render() {
     return (
