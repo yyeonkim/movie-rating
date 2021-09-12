de14647ab10519d6710b33f751ea41607907eea5
Yongyeon Kim <yeonlotus@ewhain.net>
2021-09-12 21:25:14 +0900

    Fetch movies from API
    axios로 영화 불러오기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import PropTypes from "prop-types";
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState({ count: this.state.count - 1 });
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
 
   componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("I just updated");
+    this.getMovies();
   }
 
   render() {
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
