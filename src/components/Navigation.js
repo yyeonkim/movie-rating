802d459f7c02e9bdfbd3d85ed9f0325575448b65
Yongyeon Kim <yeonlotus@ewhain.net>
2021-09-20 13:10:10 +0900

    Correct className attribute
    class --> className

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <div class="navigation">
+    <div className="navigation">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
