fd70d2ddf5a649c38dce641dfe999fe2bb8b47b0
Yongyeon Kim <yeonlotus@ewhain.net>
2021-09-20 13:24:44 +0900

    Add URL redirect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,20 @@
-export default function Detail(props) {
-  console.log(props);
-  return <h1>Movie Detail</h1>;
+import React from "react";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <h1>{location.state.title}</h1>;
+    } else {
+      return null;
+    }
+  }
 }
+
+export default Detail;
